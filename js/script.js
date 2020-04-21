d2ee4d13446a102aaf6b4cde510e07d0d899925f
elemusic <m.debski@me.com>
2020-04-21 13:34:33 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ $(window).scroll(function(){
   });
 
   $(window).scroll(function(){
-      $(".top2").css("opacity", 2.1 -$(window).scrollTop() / 150);
+      $(".top2").css("opacity", 2 -$(window).scrollTop() / 150);
     });
 
 
     $(window).scroll(function(){
-        $(".top3").css("opacity", 2.4- $(window).scrollTop() / 150);
+        $(".top3").css("opacity", 2.3- $(window).scrollTop() / 150);
       });
 
 
